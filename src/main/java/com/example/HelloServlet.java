0fe9835861ab8433783f28bca19ec65661005b70
KPgit-5 <pandeykalpana890@gmail.com>
2025-07-19 14:51:15 +0530

    Create HelloServlet.java

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        res.getWriter().println("<h1>Hello from Tomcat 10 Servlet!</h1>");
+    }
+}
